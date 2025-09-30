c7c57a511b5099713fad21ae75a94e6b7e297e16
Guilherme Galvão <gui.galvaosimplicio@gmail.com>
2025-09-30 19:39:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+int main(){
+    int opcao = 3;
+
+    switch (opcao){
+        case 1:
+            printf("Você escolheu 1\n");
+            break;
+        
+        case 2: 
+            printf("Voce escolheu 2\n");
+            break;
+
+        case 3: 
+        printf("Você escolheu 3\n");
+        break;
+
+        case 4:
+        printf("Você escolheu 4\n");
+        break;
+    }
+    return 0;
+}
